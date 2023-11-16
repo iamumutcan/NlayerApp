ad036c4943412135d99790533c90bb83a98bac90
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-11-16 21:53:14 +0300

    Core : AppDbContext, Entity Configurations

---
+++ Nlayer.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Nlayer.Core;
+
+
+namespace Nlayer.Repository.Configurations
+{
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId);
+        }
+    }
+}
