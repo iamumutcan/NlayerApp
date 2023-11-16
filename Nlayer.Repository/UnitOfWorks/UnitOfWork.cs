3ade969a10a0dea22bb0b2344e34af6fa91dbefd
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-11-17 00:54:41 +0300

    Repository : UnitOfWork

---
+++ Nlayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Nlayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Repository.UnitOfWorks
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context) { _context = context; }
+
+        public void Comit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task ComitAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
+    }
+}
