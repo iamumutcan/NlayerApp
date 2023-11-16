8e0b6f91e6ff4e6d1bde5aacacb952498adae42b
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-11-16 22:56:00 +0300

    Core : SeedData

---
+++ Nlayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Nlayer.Core;
+
+
+namespace Nlayer.Repository.Seeds
+{
+    internal class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category { Id = 1, Name = "Kalemler" },
+                new Category { Id = 2, Name = "Kitaplar" },
+                new Category { Id = 3, Name = "Defterler" });
+        }
+    }
+}
