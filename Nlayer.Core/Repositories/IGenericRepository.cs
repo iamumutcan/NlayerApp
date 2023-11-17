32a377706061aca480423747246223f75d525f37
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-11-17 15:16:05 +0300

    Service : Service Class

--- Nlayer.Core/Repositories/IGenericRepository.cs
+++ Nlayer.Core/Repositories/IGenericRepository.cs
@@ -6,7 +6,7 @@ namespace Nlayer.Core.Repositories
     public interface IGenericRepository<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
-        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetAll();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         Task AddAsync(T entity);
