cf04a0a48c493b66c9fca1fa14649298ab58f6cf
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-11-26 13:58:31 +0300

    MVC : Products Save-1

--- Nlayer.Web/Controllers/ProductsController.cs
+++ Nlayer.Web/Controllers/ProductsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nlayer.Core.DTOs;
+using Nlayer.Core.Model;
 using Nlayer.Core.Services;
 
 namespace Nlayer.Web.Controllers
@@ -6,15 +10,41 @@ namespace Nlayer.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _services;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductService services)
+        public ProductsController(IProductService services, ICategoryService categoryService, IMapper mapper)
         {
             _services = services;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task< IActionResult >Index()
         {
             return View(await _services.GetProductWithCategory());
         }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories=_categoryService.GetAllAsync();
+            var categoryiesDto= _mapper.Map<List<CategoryDto>>(categories);
+            ViewBag.Categories = new SelectList(categoryiesDto, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if(ModelState.IsValid)
+            {
+                await _services.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = _categoryService.GetAllAsync();
+            var categoryiesDto = _mapper.Map<List<CategoryDto>>(categories);
+            ViewBag.Categories = new SelectList(categoryiesDto, "Id", "Name");
+            return View();
+        }
+
     }
 }
