af407ea9842ace0003469ba0a353ae7efe828758
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-11-26 19:33:21 +0300

    MVC : FluentValidation

--- Nlayer.Web/Program.cs
+++ Nlayer.Web/Program.cs
@@ -1,15 +1,17 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Nlayer.Repository;
 using Nlayer.Service.Mapping;
+using Nlayer.Service.Validations;
 using Nlayer.Web.Modules;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
 builder.Services.AddAutoMapper(typeof(MapProfile));
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
