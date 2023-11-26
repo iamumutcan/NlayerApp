e088f94f1620dc31c5f80035102fd92e1cadbfd4
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-11-26 13:44:10 +0300

    API : ProductsController-2

---
+++ Nlayer.Web/Modules/RepoServiceModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Nlayer.Core.Repositories;
+using Nlayer.Core.Services;
+using Nlayer.Core.UnitOfWorks;
+using Nlayer.Repository.Repositories;
+using Nlayer.Repository.UnitOfWorks;
+using Nlayer.Repository;
+using Nlayer.Service.Mapping;
+using Nlayer.Service.Services;
+using System.Reflection;
+
+namespace Nlayer.Web.Modules
+{
+    public class RepoServiceModule : Autofac.Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+
+
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+        }
+    }
+}
--- Nlayer.Web/Program.cs
+++ Nlayer.Web/Program.cs
@@ -1,7 +1,29 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Nlayer.Repository;
+using Nlayer.Service.Mapping;
+using Nlayer.Web.Modules;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(MapProfile));
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
+
+
+
+builder.Host.UseServiceProviderFactory
+    (new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
 
 var app = builder.Build();
 
