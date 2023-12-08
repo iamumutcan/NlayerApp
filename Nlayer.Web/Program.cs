0d9c679dd2ace0b4bfb13e81f9a0183967cafb84
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-12-09 01:01:41 +0300

    AspNetAPI-MVC : Creating ProductApiService/CategoryApiService

--- Nlayer.Web/Program.cs
+++ Nlayer.Web/Program.cs
@@ -7,6 +7,7 @@ using Nlayer.Service.Mapping;
 using Nlayer.Service.Validations;
 using Nlayer.Web;
 using Nlayer.Web.Modules;
+using Nlayer.Web.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,14 @@ builder.Services.AddDbContext<AppDbContext>(x =>
     {
         option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
     });
+});
+builder.Services.AddHttpClient<ProductApiService>(opt =>
+{
+    opt.BaseAddress = new Uri(builder.Configuration["BaseUrl"]);
+});
+builder.Services.AddHttpClient<CategoryApiService>(opt =>
+{
+    opt.BaseAddress = new Uri(builder.Configuration["BaseUrl"]);
 });
 
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
---
+++ Nlayer.Web/Services/CategoryApiService.cs
@@ -0,0 +1,19 @@
+﻿using Nlayer.Core.DTOs;
+
+namespace Nlayer.Web.Services
+{
+    public class CategoryApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<CategoryDto>> GetAllAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<List<CategoryDto>>>("categories");
+            return response.Data;
+        }
+    }
+}
